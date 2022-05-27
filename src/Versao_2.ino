502e6d418ddffb501949b0ad50155c8425e1faa5
Rogerio B Cuenca <rogerio@cuenca.eng.br>
2022-05-27 09:11:28 -0300

    Primary Project is Completed

--- src/Versao_2.ino
+++ src/Versao_2.ino
@@ -1,42 +1,52 @@
+// ##############################################
+// # THE ROBÓTICO PROJECT - MARI AND DADDY WORK #
+// #                                            #
+// #     TKS MARI ABOUT THIS FUNNY IDEA!!!      #
+// ##############################################
+// 
+// 
 // This arduino sketch use parts of the original script of Mundo Inventivo  
 // www.youtube.com/mundoinventivo writen by Gelson Leandro Kaul
 // It'll avalible on my github repository
 
 #include <Arduino.h> //I'm using the PlatformIO IDE for VSCode, so I need to import the Arduino library
 #include <SoftwareSerial.h>
-#define rxPin 2
-#define txPin 3
-#define baudrate 9600
+
+// Pin defination: usin pin 2 and 3 to Bluetooth Module (from now, I'll refere as hc05) we have the 
+// Arduino serial port free to sync, so we don't need to disconnect the hc05 to upload the program.
+#define rxPin 2 //rx pin from hc05 - don't forget the communication of hc05 use 3.3V, so you 
+// MUST use a voltage divider to get 3.3V or 5V provided for Arduino out pin 
+#define txPin 3 //tx pin from hc05
+#define baudrate 9600 // the boudrate of communication. You can change it, but a lot of control apps use this boudrate
 
 SoftwareSerial hc05(rxPin, txPin);
  
-int IN1 = 7;
-int IN2 = 6;
-int IN3 = 5;
-int IN4 = 4;
-int PWML = 9;
-int PWMR = 10;
-int COM = 11;
-int MOT = 12;
-char comando;
+int HB_IN1 = 7;
+int HB_IN2 = 6;
+int HB_IN3 = 5;
+int HB_IN4 = 4;
+int HB_PWM_Left = 9; //PWM Pin
+int HB_PWM_Right = 10; //PWM Pin
+char Command;
  
 void setup() {
+  //here we'll define every PIN as we'll use and start the serial/bluetooth communication
 
-  Serial.begin(9600);
+  Serial.begin(9600); //start the serial port (we'll send some messages to debug the program)
   
+  // pins of hc05 
   pinMode(rxPin,INPUT);
   pinMode(txPin,OUTPUT);
-  hc05.begin(baudrate);
-  //Define os pinos como saida
 
-  pinMode(IN1, OUTPUT); 
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(PWML, OUTPUT);
-  pinMode(PWMR, OUTPUT);
-  pinMode(COM, OUTPUT);
-  pinMode(MOT, OUTPUT);
+  hc05.begin(baudrate); // start hc05
+
+  // pins of H Bridge DC motor control. We use the same name the circuit board use
+  pinMode(HB_IN1, OUTPUT); 
+  pinMode(HB_IN2, OUTPUT);
+  pinMode(HB_IN3, OUTPUT);
+  pinMode(HB_IN4, OUTPUT);
+  pinMode(HB_PWM_Left, OUTPUT);
+  pinMode(HB_PWM_Right, OUTPUT);
 }
  
 void loop() {
@@ -44,80 +54,104 @@ void loop() {
   while (hc05.available()) {
 
     if (hc05.available() > 0) {
-        Serial.println("Tem Comunicação");
+        Serial.println("Communication is Ok!"); //If the comm is enabled and working, it'll print that's ok at serial monitor
+        Serial.println(Command);
     }
-    
-    comando = hc05.read();
-    if (comando == 'F') {
-        Serial.println("Frente");
-        Serial.println(comando);
-      frente();
+    //At the first command you'll see some commented lines print the direction and de command at Serial Monitor.
+    //If you need see the other commands, you can use it as source.
+
+    Command = hc05.read();
+    if (Command == 'F') {
+        // Serial.println("Forward"); //this two lines print at screen (serial arduino screen) the procedure and command. 
+        // Serial.println(Command);
+      Forward(); //call the procedure Forward, there we'll write the commands to robot go forward
     }
-    else if (comando == 'B') {
-      Serial.println("Trás");
-      Serial.println(comando);
-      tras();
+    else if (Command == 'B') {
+      Backward();
     }
-    else if (comando == 'L') {
-      Serial.println("Esquerda");
-      Serial.println(comando);
-      esquerda();
+    else if (Command == 'L') {
+      Left();
     }
-    else if (comando == 'R') {
-      Serial.println("Direita");
-      Serial.println(comando);
-      direita();
+    else if (Command == 'R') {
+      Right();
+    }    
+    else if (Command == 'I') {
+      Right_Front();
+    }    
+    else if (Command == 'G') {
+      Left_Front();
     }
     else {
-      Serial.println("Parado");
-      Serial.println(comando);
-      parado();
+      Stoped();
     }
   }
 }
  
-void tras() {
-  //Gira o Motor A e B no sentido horario
-  analogWrite(PWML, 180);
-  analogWrite(PWMR, 180);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+void Backward() {
+  //Turn both motors on clockwise 
+  analogWrite(HB_PWM_Left, 250);
+  analogWrite(HB_PWM_Right, 250);
+  digitalWrite(HB_IN1, LOW);
+  digitalWrite(HB_IN2, HIGH);
+  digitalWrite(HB_IN3, LOW);
+  digitalWrite(HB_IN4, HIGH);
 
 }
  
-void frente() {
-  //Gira o Motor A e B no sentido anti-horario
-  analogWrite(PWML, 180);
-  analogWrite(PWMR, 180);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+void Forward() {
+  //Turn both motors on counterclockwise
+  analogWrite(HB_PWM_Left, 250);
+  analogWrite(HB_PWM_Right, 250);
+  digitalWrite(HB_IN1, HIGH);
+  digitalWrite(HB_IN2, LOW);
+  digitalWrite(HB_IN3, HIGH);
+  digitalWrite(HB_IN4, LOW);
 }
  
-void esquerda() {
-  analogWrite(PWML, 180);
-  analogWrite(PWMR, 180);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+void Left() {
+  //Turn right motor counterclockwise and left motor clockwise
+  analogWrite(HB_PWM_Left, 250);
+  analogWrite(HB_PWM_Right, 250);
+  digitalWrite(HB_IN1, LOW);
+  digitalWrite(HB_IN2, HIGH);
+  digitalWrite(HB_IN3, HIGH);
+  digitalWrite(HB_IN4, LOW);
 }
  
-void direita() {
-  analogWrite(PWML, 180);
-  analogWrite(PWMR, 180);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+void Right() {
+  //Turn left motor counterclockwise and right motor clockwise
+  analogWrite(HB_PWM_Left, 250);
+  analogWrite(HB_PWM_Right, 250);
+  digitalWrite(HB_IN1, HIGH);
+  digitalWrite(HB_IN2, LOW);
+  digitalWrite(HB_IN3, LOW);
+  digitalWrite(HB_IN4, HIGH);
 }
-void parado() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+
+void Right_Front() {
+  //Turn both motors counterclockwise but the right fastest then left
+  analogWrite(HB_PWM_Left, 70);
+  analogWrite(HB_PWM_Right, 250);
+  digitalWrite(HB_IN1, HIGH);
+  digitalWrite(HB_IN2, LOW);
+  digitalWrite(HB_IN3, HIGH);
+  digitalWrite(HB_IN4, LOW);
 }
-// O PROGRAMA DO RALABÔ TERMINA AQUI
+
+void Left_Front() {
+  //Turn both motors counterclockwise but the left fastest then lefright
+  analogWrite(HB_PWM_Left, 250);
+  analogWrite(HB_PWM_Right, 70);
+  digitalWrite(HB_IN1, HIGH);
+  digitalWrite(HB_IN2, LOW);
+  digitalWrite(HB_IN3, HIGH);
+  digitalWrite(HB_IN4, LOW);
+}
+
+void Stoped() {
+  //Stop all motors if anyother key is pressed
+  digitalWrite(HB_IN1, LOW);
+  digitalWrite(HB_IN2, LOW);
+  digitalWrite(HB_IN3, LOW);
+  digitalWrite(HB_IN4, LOW);
+}
